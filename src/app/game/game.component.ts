57aa20fbcab0b54e273e6e999ab0396878579882
Nils Boelingen <54119982+NilsBoelingen@users.noreply.github.com>
2023-12-15 14:41:34 +0100

    add some animations and logics

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  game: Game;
 
+  constructor() {
+    this.game = new Game();
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  pickCard() {
+    this.pickCardAnimation = true;
+  }
 }
--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router} from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-start-screen',
   standalone: true,
-  imports: [Router],
+  imports: [CommonModule],
   templateUrl: './start-screen.component.html',
   styleUrl: './start-screen.component.scss'
 })
 export class StartScreenComponent {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   newGame() {
     this.router.navigate(['/game']);
